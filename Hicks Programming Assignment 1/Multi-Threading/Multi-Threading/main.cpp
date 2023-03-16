8c45669d3b511c6c3bddbb40a73b522951d598b4
Joe Hicks <103856984+CEOofBeans@users.noreply.github.com>
2023-03-15 21:55:26 -0400

    Add files via upload

--- Hicks Programming Assignment 1/Multi-Threading/Multi-Threading/main.cpp
+++ Hicks Programming Assignment 1/Multi-Threading/Multi-Threading/main.cpp
@@ -1,31 +1,87 @@
 #include <iostream>
-#include <thread>
 #include <Windows.h>
 #include <process.h>
 #include <stdio.h>
-#include <math.h>
+#include "thread_counter.cpp"
 
-CRITICAL_SECTION critical_section;
+CONDITION_VARIABLE CV;
+CRITICAL_SECTION critical;
+HANDLE mutex;
 
-void thread()
+void output_c_version(int thread_num, int max, int min)
 {
-	std::cout << "Start Thread -" << GetCurrentThreadId() << std::endl;
-	std::cout << "Finish Thread -" << GetCurrentThreadId() << std::endl;
+	std::cout << "Start Thread " << thread_num << std::endl;
+
+	// Used to determain the min/max each thread counts to
+	switch (thread_num) {
+	case 1: min = 1;     max = 50;    break;
+	case 2: min = 101;   max = 150;   break;
+	case 3: min = 1001;  max = 1050;  break;
+	case 4: min = 10001; max = 10050; break;
+	}
+
+	// Prints current number
+	for(int i = min; i < max; i++)
+		std::cout << i << std::endl;
+
+	std::cout << "Finish Thread " << thread_num << std::endl << std::endl;
+}
+
+unsigned int __stdcall create_c_thread(void* arg)
+{
+	int thread_num = *(int*)arg;
+
+	EnterCriticalSection(&critical);
+	WaitForSingleObject(mutex, INFINITE);
+
+	output_c_version(thread_num, 0, 0);
+
+	ReleaseMutex(mutex);
+	LeaveCriticalSection(&critical);
+	return 0;
+}
+
+// I only included this to show the both versions have the same result
+void cpp_version()
+{
+	// C++ Verison of the multi-threading
+	std::cout << "Start Application" << std::endl;
+	ThreadCounter thread;
+	thread.create_cpp_thread(4);
+	std::cout << "End Application" << std::endl;
 	std::cout << std::endl;
 }
 
 int main()
 {
-	int counter = 0;
+	cpp_version();
+
+	// C Verison of the multi-threading
+	const int num_threads = 4;
+
 	std::cout << "Start Application" << std::endl;
-	//Loops creation of threads
-	while (counter < 4) {
-		EnterCriticalSection(&critical_section);
-		counter++;
-		thread();
+	InitializeCriticalSection(&critical);
+	mutex = CreateMutex(0, 0, 0);
+
+	HANDLE handles[num_threads];
+	int thread_args[num_threads];
+
+	for (int i = 0; i < num_threads; i++) {
+		thread_args[i] = i + 1;
+		handles[i] = (HANDLE)_beginthreadex(0, 0, &create_c_thread, &thread_args[i], 0, 0);
 	}
-	
+
+	WaitForMultipleObjects(num_threads, handles, true, INFINITE);
+
+	for (int i = 0; i < num_threads; i++) {
+		CloseHandle(handles[i]);
+	}
+
+	getchar();
+	CloseHandle(mutex);
+
 	std::cout << "End Application" << std::endl;
+	std::cout << std::endl;
+	DeleteCriticalSection(&critical);
 	return 0;
 }
-
